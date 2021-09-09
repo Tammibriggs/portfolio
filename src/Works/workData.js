03f5a3f55284a5c557d6cb3a4b8a484399bd1318
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-09-09 20:58:55 +0100

    added new work

--- src/Works/workData.js
+++ src/Works/workData.js
@@ -1,8 +1,28 @@
 import saiown_mockup from '../images/saiown_mockup.png'
 import github_repo_page from '../images/github_repo_page.png'
-
+import amazon_clone from '../images/amazon_clone.png'
 
 const workData = [
+  {
+    image: amazon_clone,
+    name: "Amazon clone",
+    tags: ["React", "Express"],
+    link: "https://challange-5f99e.web.app/",
+    description: `A clone of Amazon built with React and Express. 
+    The app includes features like User Registration, User Authentication, 
+    Stripe payment functionality, Shopping cart, and more.`
+  },
+  {
+    image: github_repo_page,
+    name: "Github Repo page",
+    tags: ["JavaScript"],
+    link: "https://github-repo-page.netlify.app/",
+    description: `
+    A clone of Github's repository page created with HTML, CSS, 
+    and JavaScript. In this app, I used Github's Graphql API to fetch 
+    the repository and profile data of the supplied Github user.
+    `
+  },
   {
     image: saiown_mockup,
     name: "Saiown",
@@ -14,19 +34,8 @@ const workData = [
       Hacker news API. The app has features like Client-side caching, 
       Pagination, Server-side searching.
     `
-  },
-
-  {
-    image: github_repo_page,
-    name: "Github Repo page",
-    tags: ["JavaScript"],
-    link: "https://github-repo-page.netlify.app/",
-    description: `
-    A clone of Github's repository page created with HTML, CSS, 
-    and JavaScript. In this app, I used Github's Graphql API to fetch 
-    the repository and profile data of the supplied Github user.
-    `
   }
+
 ]
 
 export default workData
